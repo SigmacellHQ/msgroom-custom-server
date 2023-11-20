2cf74f73c9721a5c700c41caf1d1801fa44b346a
nolanwhy <69528856+nolanwhy@users.noreply.github.com>
2023-11-20 22:32:57 +0100

    DOTN USE IT

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
+//@ts-check
 import "dotenv/config";
-import { createServer as HTTPServer } from "http";
+import express from "express";
+import { createServer as createHTTPServer } from "http";
 import { Server as SocketIOServer } from "socket.io";
 import { handle as handleFileRequests } from "./server/file_server.js";
 import { handle as handleSIORequests } from "./server/socket_server.js";
 import { handle as handleAdminAPI } from "./server/admin_api.js";
 
-const http = HTTPServer();
-const io = new SocketIOServer(http, {
-    path: "/socket.io",
-    transports: ["polling", "websocket"]
-});
+const app = express();
+const httpServer = createHTTPServer(app);
+/** @type {import("msgroom/types/socket.io")} */
+const io = new SocketIOServer(httpServer);
 
-handleAdminAPI(http);
-handleFileRequests(http);
+handleAdminAPI(httpServer);
+handleFileRequests(httpServer);
 handleSIORequests(io);
 
 const args = process.argv;
-const PORT = parseInt(args[2]) || 3030; // 0: node, 1: index.js, 2: port
+const port = parseInt(args[2]) || 3030; // 0: node, 1: index.js, 2: port
 
-console.log(`Starting on port ${PORT}...`);
-let started = false;
-try {
-    http.listen(PORT);
-    started = true;
-} catch (e) {
-    started = false;
-    console.error(e);
-}
-if (started) {
-    console.log(`Done: http://localhost:${PORT}`);
-}
+console.log(`Starting on port ${port}...`);
+app.listen(port, () => console.log(`Server online at http://localhost:${port}`));
